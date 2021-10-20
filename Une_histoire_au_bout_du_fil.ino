d343ad6bbac6410ee51aaee514160f22ebffc346
Samy Rabih <contact@lahaut.info>
2021-10-20 09:38:28 +0200

    Update Une_histoire_au_bout_du_fil.ino

--- Une_histoire_au_bout_du_fil.ino
+++ Une_histoire_au_bout_du_fil.ino
@@ -1,8 +1,11 @@
-/* Gestion CSV */
+const char string_0[] PROGMEM = "Le petit princeLe petit princeLe petit princeLe petit prince"; // "String 0" etc are strings to store - change to suit.
+const char string_1[] PROGMEM = "Le petit princeLe petit princeLe petit princeLe petit prince";
+const char string_2[] PROGMEM = "Le petit princeLe petit princeLe petit princeLe petit prince";
+const char string_3[] PROGMEM = "Le petit princeLe petit princeLe petit princeLe petit prince";
+const char string_4[] PROGMEM = "Le petit princeLe petit princeLe petit princeLe petit prince";
+const char string_5[] PROGMEM = "Le petit princeLe petit princeLe petit princeLe petit prince";
+const char *const titles[] PROGMEM = {string_0, string_1, string_2, string_3, string_4, string_5};
 
-char** MP3Authors;
-char** MP3Titles;
-bool hasReadCSV = false;
 
 /* Lecteur MP3 */
 #include <SoftwareSerial.h>
@@ -15,20 +18,19 @@ bool hasReadCSV = false;
 #include "OLED_GUI.h"
 #include "DEV_Config.h"
 #include "Show_Lib.h"
-#include "Debug.h"
 bool isScreenInitialized = false;
 
 /* Boutons */
-const int buttonCount = 10;                              //Nombre de boutons
+const int buttonCount PROGMEM = 10;                              //Nombre de boutons
 int buttonStates[buttonCount];                           //Etat des boutons
 int lastButtonPressedIndex = -1;                         //Numéro du dernier bouton pressé
-int buttonsPins[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };  //Liste des ports sur lesquels sont branchés les boutons
-int powerPin = 12;
+const int buttonsPins[] PROGMEM = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };  //Liste des ports sur lesquels sont branchés les boutons
+const int powerPin PROGMEM = 12;
 bool isAButtonPressed = false;
 bool isADifferentButtonPressed = false;
 bool areButtonsInitialized = false;
-int MP3_PIN1 = A6;  //Le port de l'Ardunino branché sur le TX du DFPlayer
-int MP3_PIN2 = A5;  //Le port de l'Ardunino branché sur le RX du DFPlayer
+const int MP3_PIN1 PROGMEM = A6;  //Le port de l'Ardunino branché sur le TX du DFPlayer
+const int MP3_PIN2 PROGMEM = A5;  //Le port de l'Ardunino branché sur le RX du DFPlayer
 SoftwareSerial MP3Serial(MP3_PIN1, MP3_PIN2);
 DFRobotDFPlayerMini mp3Player;
 
@@ -38,18 +40,13 @@ void setup() {
   MP3Serial.begin(9600);
   mp3Player.begin(MP3Serial);
   mp3Player.volume(25);
-<<<<<<< HEAD
-  Serial.begin(9600);
-=======
->>>>>>> d69fc2cbd0474826006fcac157f5d71a940bee77
+  char valeur[64]; // Attention : La variable doit être suffisamment grande pour stocker le message
+  strcpy_P(valeur, (char*) pgm_read_word(&(titles[2])));
 }
 
 void loop() {
-  if (!hasReadCSV) {
-    readFromSd();
-  }
   if (!isScreenInitialized) {
-    Ecran();
+    initScreen();
   }
   if (!checkPower()) {
     return;
@@ -57,7 +54,6 @@ void loop() {
   if (!areButtonsInitialized) {
     initializeButtons();
   }
-  Serial.println("ok");
   handleButtons();
 }
 
@@ -99,12 +95,9 @@ void handleButtons() {
   isADifferentButtonPressed = false;
   for (int i = 0; i < buttonCount; i++) {
     if (digitalRead(buttonsPins[i]) == LOW) {  //Dans le cas de INPUT_PULLUP, c'est la valeur LOW qui indique que le port est alimenté
-      Serial.println((String) "Bouton :" + i);
-      Serial.println((String) "lastButtonPressedIndex :" + lastButtonPressedIndex);
       isAButtonPressed = true;
       //Inutile de retester 60 fois / seconde si un bouton est enfoncé :)
       if (lastButtonPressedIndex != i || lastButtonPressedIndex == -1) {
-        Serial.println("A different button has been pressed1");
 
         isADifferentButtonPressed = true;
       }
@@ -117,11 +110,9 @@ void handleButtons() {
 
   //Si un bouton a été pressé
   if (isAButtonPressed) {
-    Serial.println("A button has been pressed");
     //Si c'est un autre bouton que celui d'avant, on arrête la lecture seulement si elle est en cours
     //Valable aussi au premier bouton
     if (isADifferentButtonPressed) {
-      Serial.println("A different button has been pressed2");
       if (-1 != mp3Player.readState()) {
         mp3Player.pause();
       }
@@ -137,7 +128,7 @@ void handleButtons() {
   }
 }
 
-void Ecran() {
+void initScreen() {
   System_Init();
 
   OLED_SCAN_DIR OLED_ScanDir = SCAN_DIR_DFT;
@@ -147,42 +138,10 @@ void Ecran() {
   OLED_ClearBuf();
   OLED_ClearScreen(OLED_BACKGROUND);
 
-  GUI_DisString_EN(0 , 2, "I'M ALIVE !!", &Font16, FONT_BACKGROUND, WHITE);
+  GUI_DisString_EN(0 , 2, "Boite a histoires", &Font16, FONT_BACKGROUND, WHITE);
 
   OLED_Display(0, 65, 128, 65 + 32);
   OLED_ClearBuf();
   isScreenInitialized = true;
 
 }
-
-/* Lit le CSV depuis la carte SD et stocke les données pour chaque colonne/ligne */
-void readFromSd() {
-<<<<<<< HEAD
-  hasReadCSV = true;
-}
-=======
-
-  SdFat SD;
-  File dir;
-  File file;
-  int SD_PIN = A0;
-
-#define SD_CONFIG SdSpiConfig(SD_PIN)
-#define error(s) SD.errorHalt(&Serial, F(s))
-
-  // Initialize the SD.
-  if (!SD.begin(SD_CONFIG)) {
-    Serial.println("BAD");
-
-    SD.initErrorHalt(&Serial);
-  }
-  // Create the file.
-  if (!file.open("liste.csv", FILE_READ)) {
-    error("open failed");
-  }
-
-  // Rewind file for read.
-  file.rewind();
-  hasReadCSV = true;
-}
->>>>>>> d69fc2cbd0474826006fcac157f5d71a940bee77
