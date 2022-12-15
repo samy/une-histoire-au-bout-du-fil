d71dff84dbb4f4df9add9f3694d6f0288cb0d038
Samy RABIH <samy.rabih@hubicus.com>
2022-12-15 21:58:22 +0100

    Debug pour Teensy

--- Livre_dor_T4/Livre_dor_T4.ino
+++ Livre_dor_T4/Livre_dor_T4.ino
@@ -1,17 +1,6 @@
-
 #include <SdFat.h>
 
-// SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
-// 1 for FAT16/FAT32, 2 for exFAT, 3 for FAT16/FAT32 and exFAT.
 #define SD_FAT_TYPE 1
-/*
-  Change the value of SD_CS_PIN if you are using SPI and
-  your hardware does not use the default value, SS.
-  Common values are:
-  Arduino Ethernet shield: pin 4
-  Sparkfun SD shield: pin 8
-  Adafruit SD shields and modules: pin 10
-*/
 
 // SDCARD_SS_PIN is defined for the built-in SD on some boards.
 #ifndef SDCARD_SS_PIN
@@ -33,84 +22,60 @@ const uint8_t SD_CS_PIN = SDCARD_SS_PIN;
 #define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_CLOCK)
 #endif  // HAS_SDIO_CLASS
 
-#if SD_FAT_TYPE == 0
-SdFat sd;
-File file;
-#elif SD_FAT_TYPE == 1
 SdFat32 sd;
 File32 file;
-#elif SD_FAT_TYPE == 2
-SdExFat sd;
-ExFile file;
-#elif SD_FAT_TYPE == 3
-SdFs sd;
-FsFile file;
-#else  // SD_FAT_TYPE
-#error Invalid SD_FAT_TYPE
-#endif  // SD_FAT_TYPE
+
+#define RECORDS_NUMBER_FILE_NAME ".records_file_number"
+#define INTRO_RECORD_ENABLE true
+#define INTRO_PLAY_ENABLE false
+
+#define PIN_HANG 10  //Port lié au fil du décrochage
+
+char PHONE_MODE[10] = "record";
+char introHasBeenPlayed=false;
 
 char line[40];
+char recordsNumber[10];
+char* tmpContent;
+int phoneStatus = 0;
+
 
 //------------------------------------------------------------------------------
 // Store error strings in flash to save RAM.
 #define error(s) sd.errorHalt(&Serial, F(s))
-//------------------------------------------------------------------------------
-// Check for extra characters in field or find minus sign.
-char* skipSpace(char* str) {
-  while (isspace(*str)) str++;
-  return str;
-}
-//------------------------------------------------------------------------------
-bool parseLine(char* str) {
-  char* ptr;
-
-  // Set strtok start of line.
-  str = strtok(str, ",");
-  if (!str) return false;
-
-  // Print text field.
-  Serial.println(str);
-
-  // Subsequent calls to strtok expects a null pointer.
-  str = strtok(nullptr, ",");
-  if (!str) return false;
-
-  // Convert string to long integer.
-  int32_t i32 = strtol(str, &ptr, 0);
-  if (str == ptr || *skipSpace(ptr)) return false;
-  Serial.println(i32);
-
-  str = strtok(nullptr, ",");
-  if (!str) return false;
-
-  // strtoul accepts a leading minus with unexpected results.
-  if (*skipSpace(str) == '-') return false;
-
-  // Convert string to unsigned long integer.
-  uint32_t u32 = strtoul(str, &ptr, 0);
-  if (str == ptr || *skipSpace(ptr)) return false;
-  Serial.println(u32);
-
-  str = strtok(nullptr, ",");
-  if (!str) return false;
-
-  // Convert string to double.
-  double d = strtod(str, &ptr);
-  if (str == ptr || *skipSpace(ptr)) return false;
-  Serial.println(d);
-
-  // Check for extra fields.
-  return strtok(nullptr, ",") == nullptr;
-}
+//-----------------
 //------------------------------------------------------------------------------
 void setup() {
+  initEnvironnement();
+}
+
+void loop() {
+  /* Si le téléphone est raccroché, on stoppe la lecture du MP3 (il n'a pas de véritable stop() et on passe à l'itération suivante */
+  if (isHangedUp()) {
+    //myDFPlayer.pause();
+
+    phoneStatus = 0;
+    return;
+  } else {
+    if (phoneStatus == 0) {
+      phoneStatus = 1;
+    }
+  }
+  if (phoneStatus == 1 && needToPlayIntro()) {
+    phoneStatus = 2;
+    //playIntro();
+  }
+  phoneStatus = 2;
+}
+
+void initEnvironnement() {
+
   Serial.begin(9600);
 
   // Wait for USB Serial
   while (!Serial) {
     yield();
   }
-  Serial.println("Type any character to start");
   while (!Serial.available()) {
     yield();
   }
@@ -119,41 +84,54 @@ void setup() {
     sd.initErrorHalt(&Serial);
     return;
   }
-  // Remove any existing file.
-  if (sd.exists(".test")) {
-    sd.remove(".test");
-  }
-  // Create the file.
-  if (!file.open(".test", FILE_WRITE)) {
-    error("open failed");
-  }
-  int bits = file.attrib();              // should test for -1 here.
-  file.attrib(bits | FS_ATTRIB_HIDDEN);  // should test return
-  // Write test data.
-  file.print(F(
-    "abc,123,456,7.89\r\n"
-    "def,-321,654,-9.87\r\n"
-    "ghi,333,0xff,5.55"));
-
-  // Rewind file for read.
-  file.rewind();
-
-  while (file.available()) {
-    int n = file.fgets(line, sizeof(line));
-    if (n <= 0) {
-      error("fgets failed");
-    }
-    if (line[n - 1] != '\n' && n == (sizeof(line) - 1)) {
-      error("line too long");
-    }
-    if (!parseLine(line)) {
-      error("parseLine failed");
-    }
-    Serial.println();
-  }
-  file.close();
-  Serial.println(F("Done"));
 }
 
-void loop() {
+char* getRecordsNumber() {
+  if (!file.open(RECORDS_NUMBER_FILE_NAME, FILE_WRITE)) {
+    error("open failed");
+  }
+
+  int bits = file.attrib();
+  file.attrib(bits | FS_ATTRIB_HIDDEN);
+
+  file.rewind();
+  tmpContent = 0;
+  while (file.available()) {
+    tmpContent = file.read();
+  }
+  if (sizeof(tmpContent) == 0) {
+    tmpContent = 0;
+  }
+  file.close();
+  return tmpContent;
+}
+
+void setRecordsNumber(char number) {
+  if (!file.open(RECORDS_NUMBER_FILE_NAME, FILE_WRITE)) {
+    error("open failed");
+  }
+
+  int bits = file.attrib();
+  file.attrib(bits | FS_ATTRIB_HIDDEN);
+
+  file.truncate();
+  file.print(number);
+  file.close();
+}
+
+bool needToPlayIntro() {
+  if (introHasBeenPlayed) {
+    return false;
+  }
+  if ((PHONE_MODE == "record" && INTRO_RECORD_ENABLE)
+      || (PHONE_MODE == "play" && INTRO_PLAY_ENABLE)) {
+    introHasBeenPlayed = true;
+    return true;
+  }
+  return false;
+}
+
+/* Récupération de l'état de décroché/raccroché */
+bool isHangedUp() {
+  return 1 == digitalRead(PIN_HANG);
 }
