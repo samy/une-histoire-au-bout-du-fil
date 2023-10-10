8efccedaaa60e1188be70f28c44b5cdb92db1c28
Samy Rabih <contact@lahaut.info>
2023-10-10 22:30:38 +0200

    Stockage des tags dans une liste

--- Stethoscope/Stethoscope.ino
+++ Stethoscope/Stethoscope.ino
@@ -7,7 +7,8 @@ PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
 String tagId = "None";
 String newTagId = "None";
-byte nuidPICC[4];
+const char* tags[3] = { "uno", "duo", "tri" };
+
 
 void setup(void) {
   Serial.begin(115200);
@@ -26,13 +27,23 @@ void readNFC() {
     NfcTag tag = nfc.read();
     //tag.print();
     newTagId = tag.getUidString();
+    Serial.println(findNumberForTag(newTagId));
     if (tagId != newTagId) {
       tagId = newTagId;
-
       Serial.print("Tag RFID trouvé : ");
       Serial.println(tagId);
       Serial.println(' ');
     }
   }
   delay(100);
+}
+
+int findNumberForTag(String tagKey) {
+  int tagIndex = 0;
+  for (tagIndex = 0; tagIndex < sizeof(tags) / sizeof(tags[0]); tagIndex++) {
+    if (strcmp(tags[tagIndex], tagKey.c_str()) == 0) {
+      break;
+    }
+  }
+  return tagIndex;
 }
